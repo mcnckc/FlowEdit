3a4408cae068ec98a5ac23c6ce4aac5ddb6d1753
mcnckc <s20b1_gusev@179.ru>
2025-04-10 00:25:42 +0300

    test offload

--- run_script.py
+++ run_script.py
@@ -40,8 +40,16 @@ if __name__ == "__main__":
     else:
         raise NotImplementedError(f"Model type {model_type} not implemented")
     #pipe.enable_sequential_cpu_offload()
-    apply_group_offloading(pipe.transformer, onload_device=device, offload_device=torch.device('cpu'), 
-                                          offload_type="block_level", num_blocks_per_group=11, use_stream=True)
+    pipe.scheduler = pipe.scheduler.to(device)
+    pipe.text_encoder = pipe.text_encoder.to(device)
+    pipe.text_encoder_2 = pipe.text_encoder_2.to(device)
+    pipe.tokenizer = pipe.tokenizer.to(device)
+    pipe.tokenizer_2 = pipe.tokenizer_2.to(device)
+    pipe.tokenizer_3 = pipe.tokenizer_3.to(device)
+    pipe.transformer = pipe.transformer.to(device)
+    pipe.vae = pipe.vae.to(device)
+    #apply_group_offloading(pipe.transformer, onload_device=device, offload_device=torch.device('cpu'), 
+    #                                      offload_type="block_level", num_blocks_per_group=11, use_stream=True)
     scheduler = pipe.scheduler
     #pipe = pipe.to(device)
 
