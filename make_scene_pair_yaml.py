b27a4950fe29fb82f91b7844747a2eff665f9653
mcnckc <s20b1_gusev@179.ru>
2025-04-14 19:30:02 +0300

    other prompt

--- make_scene_pair_yaml.py
+++ make_scene_pair_yaml.py
@@ -2,7 +2,7 @@ import os
 import random
 
 with open('i_s.txt', 'r') as sf, open("target_labels.txt", 'r') as tf, open('scene_pair_small.yaml', 'w') as res:
-    subset = 10
+    subset = 3
     if subset >= 0:
         lines = random.choices(list(zip(sf.readlines(), tf.readlines())), k=subset)
     else:
@@ -13,10 +13,10 @@ with open('i_s.txt', 'r') as sf, open("target_labels.txt", 'r') as tf, open('sce
         res.write(f"-\n"
                   f"    input_img: scenepair/{filename}\n"
                   f"    source_prompt: >-\n"
-                  f"        Text that reads \"{slabel}\"\n"
+                  f"        Label {slabel}\n"
                   f"    target_prompts:\n"
                   f"    - >-\n"
-                  f"        Text that reads \"{tlabel}\"\n"
+                  f"        Label {tlabel}\n"
                   f"    target_codes:\n"
                   f"    - >-\n"
                   f"        {filename}-{slabel}-to-{tlabel}\n")
