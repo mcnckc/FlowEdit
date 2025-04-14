1794d99916ec7402013688c446996c766bbfe410
mcnckc <s20b1_gusev@179.ru>
2025-04-14 17:58:49 +0300

    add small data

--- make_scene_pair_yaml.py
+++ make_scene_pair_yaml.py
@@ -1,7 +1,13 @@
 import os
+import random
 
-with open('i_s.txt', 'r') as sf, open("target_labels.txt", 'r') as tf, open('scene_pair.yaml', 'w') as res:
-    for sline, tline in zip(sf, tf):
+with open('i_s.txt', 'r') as sf, open("target_labels.txt", 'r') as tf, open('scene_pair_small.yaml', 'w') as res:
+    subset = 10
+    if subset >= 0:
+        lines = random.choices(list(zip(sf.readlines(), tf.readlines())), k=subset)
+    else:
+        lines = zip(sf.readlines(), tf.readlines())
+    for sline, tline in lines:
         filename, slabel = sline.split()
         tlabel = tline.split()[1]
         res.write(f"-\n"
